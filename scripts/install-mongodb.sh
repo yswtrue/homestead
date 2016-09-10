1c5ae5037db4474df8976f18318e933d8eb5e2ca
ysw <ysw@yswdeMac-mini.lan>
2016-09-10 11:07:16 +0800

    mongodb install script

--- scripts/install-mongodb.sh
+++ scripts/install-mongodb.sh
@@ -2,11 +2,24 @@
 #!/bin/bash
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
 apt-get update
 
 apt-get install -y mongodb-org
+
+echo "[Unit]
+Description=High-performance, schema-free document-oriented database
+After=network.target
+Documentation=https://docs.mongodb.org/manual
+
+[Service]
+User=mongodb
+Group=mongodb
+ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
+
+[Install]
+WantedBy=multi-user.target" > /lib/systemd/system/mongod.service
 
 sed -i "s/bindIp:/#&/g" /etc/mongod.conf
 
